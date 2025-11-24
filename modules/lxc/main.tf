06230a042c4a6a17600af348e50ec5c233ae9672
Antonino Parisi <tabman83@gmail.com>
2025-11-24 19:22:06 +0000

    Create main.tf

---
+++ modules/lxc/main.tf
@@ -0,0 +1,27 @@
+resource "proxmox_lxc" "this" {
+  hostname     = var.hostname
+  vmid         = var.vmid
+  target_node  = var.node
+
+  ostemplate   = var.ostemplate
+  unprivileged = true
+  onboot       = true
+
+  cores   = var.cpu_cores
+  memory  = var.memory_mb
+  swap    = 0
+
+  rootfs {
+    storage = var.storage
+    size    = var.disk_size
+  }
+
+  network {
+    name   = "eth0"
+    bridge = var.bridge
+    ip     = var.ip
+    ip6    = "auto"
+  }
+
+  tags = length(var.tags) > 0 ? join(";", var.tags) : null
+}
