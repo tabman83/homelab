5efb4ca264d717878b1dfbcf1c8e9bcd9bf36239
Antonino Parisi <tabman83@gmail.com>
2025-11-26 16:06:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
 
   required_providers {
     proxmox = {
-      source = "Telmate/proxmox"
+      source = "telmate/proxmox"
       # version = "~> 3.0" # optional pin, you can uncomment later
     }
   }
