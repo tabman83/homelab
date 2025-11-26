4ca5bb008dc11e8aba33368df107d02bdd6d9dde
Antonino Parisi <tabman83@gmail.com>
2025-11-26 20:11:06 +0000

    Update main.tf

--- modules/lxc/main.tf
+++ modules/lxc/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.66"
+    }
+  }
+}
+
 resource "proxmox_lxc" "this" {
   hostname     = var.hostname
   vmid         = var.vmid
