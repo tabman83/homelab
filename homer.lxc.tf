cb38a5031741eccbd64dde3d6f3a0bdf2724ad66
Antonino Parisi <tabman83@gmail.com>
2025-11-27 01:39:26 +0000

    Update homer.lxc.tf

--- homer.lxc.tf
+++ homer.lxc.tf
@@ -7,10 +7,10 @@ module "homer_lxc" {
   ostemplate      = "local:vztmpl/debian-13-standard_13.1-2_amd64.tar.zst"
   unprivileged    = true
   onboot          = true
-  cores           = 2
+  cores           = 1
   memory          = 1024
   rootfs_storage  = "ceph-rbd-homelab"
-  rootfs_size     = "20"
+  rootfs_size     = "1"
   ip_address      = "192.168.5.51/24"
   gateway         = "192.168.5.1"
   network_bridge  = "vmbr0"
